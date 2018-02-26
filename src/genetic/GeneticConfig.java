f49d7974da22663758fd86697dd1e6724679cbda
Flo Schenk <ch3-cooh@web.de>
2018-02-26 13:29:05 +0100

    changed GeneticConfig

---
+++ src/genetic/GeneticConfig.java
@@ -0,0 +1,10 @@
+package genetic;
+
+public class GeneticConfig {
+    public static final int MAX_ITERATIONS = 40000;
+    public static final int MAX_FITNESS = 46;
+    public static final int SIZE_OF_POPULATION =2000;
+    public static final int PROBABILITY_OF_MUTATION_IN_PERCENT = 50;
+    public static final int NUMBER_OF_MUTATIONS = 10;
+    public static final String TYPE_OF_CROSSOVER = "modulo";
+}
